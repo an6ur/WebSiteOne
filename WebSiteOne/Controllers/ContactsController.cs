a7fc048fac7e2d4d322267933ecc693a97efde81
an6ur <theslyfox47@gmail.com>
2023-05-28 14:03:13 +0300

    Добавьте файлы проекта.

---
+++ WebSiteOne/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSiteOne.Models;
+
+namespace WebSiteOne.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() // contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebSiteOne/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebSiteOne.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+        [Display(Name = "Введите имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам нужно ввести почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+        [StringLength(30, ErrorMessage = "Напишите сообщение меньше 30 символов!")]
+        public string Message { get; set; }
+    }
+}
